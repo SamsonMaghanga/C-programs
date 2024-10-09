6b47879ef18ee47aae7f174d526d4789c576c54e
Samson Maghanga <maghangasamson02@gmail.com>
2024-10-09 07:11:49 +0300

    first commit
    Demonstration while loop

---
+++ While loop.c.c
@@ -0,0 +1,14 @@
+// Demenstration For While Loop
+#include <stdio.h>
+
+int main()
+{
+	int x=100;
+	while(x>0)	{
+	printf(" %d " ,x);
+	x=x-1;	
+	}	
+	printf("Loop body finished");
+	
+	return 0;}
+	
