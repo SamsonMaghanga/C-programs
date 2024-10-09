f2be5ba4dc9237ce3b7358973f090e050a389cf0
Samson Maghanga <maghangasamson02@gmail.com>
2024-10-09 07:03:20 +0300

    Add files via upload

---
+++ October.c/For loop.C
@@ -0,0 +1,15 @@
+// Demenstration For Loop
+#include <stdio.h>
+
+int main(){
+	int i, sum=0;
+	//for(start;stop;step)
+	for(i=100;i>0;i--){
+		printf("%d \n", i);
+		sum = sum + i;
+		//sum +=i;
+	}	
+	printf("The sum is %d" , sum);
+	
+	return 0;}
+	
